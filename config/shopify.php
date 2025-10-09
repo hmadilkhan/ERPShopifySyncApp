f9366fdad9c78e76c0c45286fed6b4ae98ea30ac
hmadilkhan <adilsziu@outlook.com>
2025-10-09 13:01:53 +0500

    Shopify

--- config/shopify.php
+++ config/shopify.php
@@ -3,7 +3,7 @@
 return [
     'api_key'       => env('SHOPIFY_API_KEY'),
     'api_secret'    => env('SHOPIFY_API_SECRET'),
-    'scopes'        => env('SHOPIFY_SCOPES', 'read_orders,write_orders'),
+    'scopes'        => env('SHOPIFY_SCOPES', 'read_orders,write_orders,read_fulfillments,write_fulfillments,read_assigned_fulfillment_orders,write_assigned_fulfillment_orders'),
     'redirect_uri'  => env('SHOPIFY_REDIRECT_URI'),
     'api_version'   => env('SHOPIFY_API_VERSION', '2025-01'),
 ];
