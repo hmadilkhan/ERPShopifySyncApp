314e5dc1d52ff35dab7156bfe1124db3e076d5df
hmadilkhan <adilsziu@outlook.com>
2025-10-09 15:37:31 +0500

    Migration ereror resovled

--- database/migrations/2025_10_09_091017_add_location_id_into_shopify_shops_table.php
+++ database/migrations/2025_10_09_091017_add_location_id_into_shopify_shops_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('shopify_shops', function (Blueprint $table) {
-            $table->dropColumn('shopify_location_id')
+            $table->dropColumn('shopify_location_id');
         });
     }
 };
